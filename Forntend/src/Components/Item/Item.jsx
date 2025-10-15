4f6fb602305be2d7e333eb7d58e59ab95e845aee
Ankit kumar <ankitkumar62601@gmail.com>
2025-10-15 15:58:38 +0530

    Update Item.jsx

--- Forntend/src/Components/Item/Item.jsx
+++ Forntend/src/Components/Item/Item.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import './item.css'
+
 const Item = (props) => {
   return (
     <div className='item'>
         <h2>{props.category}</h2>
-      <Link to={`/product/${props.id}`}><img onClick={window.scrollTo(0,0)} src={props.img} alt="" /></Link>
+      <Link to={`/product/${props.id}`}><img onClick={() => window.scrollTo(0,0)} src={props.img} alt="" /></Link>
       <p className='name'>{props.name}</p>
       <div className='item-prices'>
         <p className='new_price'>${props.new_price}</p>
         <p className='old_price'>${props.old_price}</p>
-        </div>
+      </div>
     </div>
   )
 }
